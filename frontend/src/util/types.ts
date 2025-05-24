4091683c81bcde66206b8668d8d7ecdc6cd8d3b9
Yumeng Liu <lym20041026@gmail.com>
2025-05-24 13:59:16 -0700

    added type

--- frontend/src/util/types.ts
+++ frontend/src/util/types.ts
@@ -1,13 +1,34 @@
+import { UUID } from "crypto";
+import { User } from "firebase/auth";
+
 export type UserData = {
-  availability?: Record<string, { start: string; end: string }>
-  name: string
-  email?: string
-  major?: string
-  year?: string
-  profileURL?: string
-  createdAt?: string
-  interests: string[]
-  uid?: string
-}
+	availability?: Record<string, { start: string; end: string }>;
+	name: string;
+	email?: string;
+	major?: string;
+	year?: string;
+	profileURL?: string;
+	createdAt?: string;
+	interests: string[];
+	uid?: string;
+};
 
+export type JointRecommendedEvent = {
+	type: string;
+	name: string;
+	description: string;
+	time?: string;
+	location: string;
+	url?: string;
+	organizer?: string;
+	items_needed?: string;
+};
 
+export type JointRecommendationOutput = {
+	recommendation: JointRecommendedEvent;
+	message: string;
+};
+
+export type Hangout = JointRecommendedEvent & { users: UserData[];
+  id: UUID
+ };
