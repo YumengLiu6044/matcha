2380ae29ad99ab50834eab23537cb8dfe10dc639
Yumeng Liu <lym20041026@gmail.com>
2025-05-24 17:35:24 -0700

    fixed link bug

--- frontend/src/components/dashboard-nav.tsx
+++ frontend/src/components/dashboard-nav.tsx
@@ -121,7 +121,7 @@ export default function DashboardNav() {
               </DropdownMenuItem>
               <DropdownMenuItem asChild>
                 <Link
-                  href="/dashboard/settings"
+                  href="/dashboard"
                   className="flex w-full cursor-pointer items-center"
                 >
                   <Settings className="mr-2 h-4 w-4" />
@@ -134,7 +134,7 @@ export default function DashboardNav() {
                 onClick={async () => {
                   const auth = getAuth(app);
                   await signOut(auth);
-                  window.location.href = "/home";
+                  window.location.href = "/";
                 }}
               >
                 <Link
